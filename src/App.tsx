9e364f8737ab29a781d172e266946c9833a73b20
Harsh Goel <harshgoel05@gmail.com>
2022-10-02 10:45:26 +0530

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Create from './pages/Create';
+import Home from './pages/Home';
+import Event from './pages/Event';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="text-center">
-      <h3>Hello</h3>
+    <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="create" element={ <Create/> } />
+        <Route path="event" element={ <Event/> } />
+      </Routes>
     </div>
+     </BrowserRouter>
   );
 }
 
